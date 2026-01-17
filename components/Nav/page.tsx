31600afd4f3d455d699388095f434bc4e5575d0a
apox-rider <apolinaryavith@gmail.com>
2026-01-17 10:55:31 +0300

    added

--- components/Nav/page.tsx
+++ components/Nav/page.tsx
@@ -44,7 +44,7 @@ export default function Nav() {
           onClick={() => setIsOpen(!isOpen)}
           className="md:hidden text-slate-800 focus:outline-none"
         >
-          <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <svg className="w-8 h-8 text-blue-900 hover:text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
